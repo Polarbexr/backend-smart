c014803617fb77591c3944fafbcef580d58364b1
Polarbexr <2121100624@soy.utj.edu.mx>
2024-03-25 11:51:25 -0600

    Prueba de cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const http = require("http")
+const ourCors = "*"
 
 const express = require('express');
 const app = express();
@@ -8,7 +9,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 app.use(cors({
-    origin: '*'
+    origin: ourCors
   }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
